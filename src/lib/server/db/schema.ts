63cdac65ed6c6574f48ef99b14fda04db5aada94
Pedro Santiago <santiagopedrodev@gmail.com>
2025-02-10 13:06:01 -0300

    add uuid dependency and update database schema with new tables and UUID support

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,42 @@
-import { mysqlTable, serial, text, int } from 'drizzle-orm/mysql-core';
+import {
+	mysqlTable,
+	text,
+	varchar,
+	int,
+	decimal,
+	date
+} from 'drizzle-orm/mysql-core';
 
-export const user = mysqlTable('user', {
-	id: serial('id').primaryKey(),
-	age: int('age')
+import { v4 as uuidv4 } from 'uuid';
+
+export const category = mysqlTable('Categoria', {
+	id: int('id').primaryKey().autoincrement(),
+	name: text('nome').notNull(),
+	type: text('tipo', { enum: ['entrada', 'saida'] }).notNull()
+});
+
+export const payment_method = mysqlTable('MetodoPagamento', {
+	id: int('id').primaryKey().autoincrement(),
+	name: text('name').notNull()
+});
+
+export const vendedora = mysqlTable('Vendedora', {
+	id: varchar('id', { length: 36 }).primaryKey().$defaultFn(uuidv4),
+	name: text('nome').notNull(),
+	phone: text('telefone').notNull(),
+	address: text('endereco').notNull()
+});
+
+export const transaction = mysqlTable('Transacao', {
+	id: varchar('id', { length: 36 }).primaryKey().$defaultFn(uuidv4),
+	payer_id: varchar('pagador_id', { length: 36 }).references(
+		() => vendedora.id
+	),
+	amount: decimal('valor', { precision: 2 }).notNull(),
+	category_id: int('categoria_id').references(() => category.id),
+	payment_method_id: int('metodo_pagamento_id').references(
+		() => payment_method.id
+	),
+	date: date('data').notNull(),
+	description: text('descricao')
 });
